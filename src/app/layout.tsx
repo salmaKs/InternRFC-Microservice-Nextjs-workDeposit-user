8930bb393e2a6502f845143f5df8722764a0fe1d
salmaks <salma.ksantini@esprit.tn>
2024-08-19 12:14:23 +0100

    last commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import {Providers} from './providers'
 import { fonts } from './fonts'
-import Footer from '@/component/footer/Footer';
 import Navbar from '@/component/navbar/Navbar';
 
 const inter = Inter({ subsets: ["latin"] });
@@ -27,7 +26,6 @@ export default function RootLayout({
         <Providers>
           <Navbar/>
         <main>{children}</main>
-           <Footer/>
         </Providers>
       </body>
     </html>
